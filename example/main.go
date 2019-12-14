28fcde8ab97ead77a30010321f27431e5c8e6a7b
54mch4n <54mch4n.1111@gmail.com>
2019-12-15 06:42:08 +0900

    fix typo.

--- example/main.go
+++ example/main.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	"fmt"
-	
+
 	"github.com/54mch4n/dedup"
 )
 
 func main() {
-	ded := dedup.NewDeduplicateion()
+	ded := dedup.NewDeduplication()
 	sliceStr := []string{"Go", "V", "Java", "Python", "Go", "Ruby", "Go", "V"}
 	fmt.Println(ded.Do(sliceStr).Str())
-	
+
 	sliceInt := []int{1, 1, 2, 2, 3, 3}
 	fmt.Println(ded.Do(sliceInt).Int())
 }
